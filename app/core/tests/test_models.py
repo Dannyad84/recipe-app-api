bf0620754cd8ecbedd0a12a0941fa0249f6d0858
Dannyad84 <daniel@xanderdd.com>
2019-01-21 15:14:21 +0000

    Spaces

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,9 +5,9 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is test"""
+        """Test creating a new user with an email is successful"""
         email = 'test@londonappdev.com'
-        password= 'Testpass123'
+        password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -23,7 +23,7 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
-    def test_new_user_invlaid_email_normalized(self):
+    def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
